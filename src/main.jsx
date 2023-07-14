cc59da3ba890641d467e4aee08ef955641d409f7
Rick Vermeil <rickv85@gmail.com>
2023-07-14 06:51:40 -0600

    Reinstall Vercel analytics npm pkg, inject conditionally on main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from "react-router-dom";
 import './index.css';
 import App from './Components/App/App';
+import { inject } from '@vercel/analytics';
+
+if (!window.Cypress && window.location.host !== 'localhost:5173') {
+  inject();
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
