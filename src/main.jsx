c145a92353953b108681e37e0363eb50c783f48d
Rick Vermeil <rickv85@gmail.com>
2023-10-25 09:01:54 -0600

    Add gstatic to both policies

--- src/main.jsx
+++ src/main.jsx
@@ -14,9 +14,9 @@ let cspContent = "";
 const environment = import.meta.env.VITE_ENV;
 
 if (environment === 'development') {
-  cspContent = `default-src 'self' 'unsafe-inline'; script-src 'self' 'unsafe-inline' http://localhost:5173; img-src 'self' http://localhost:5173; connect-src 'self' http://localhost:5173 http://localhost:5001 http://www.strava.com ; font-src 'self'; style-src 'self';`;
+  cspContent = `default-src 'self' 'unsafe-inline'; script-src 'self' 'unsafe-inline' http://localhost:5173; img-src 'self' http://localhost:5173; connect-src 'self' http://localhost:5173 http://localhost:5001 http://www.strava.com ; font-src 'self' https://fonts.gstatic.com/; style-src 'self' https://fonts.gstatic.com/;`;
 } else {
-  cspContent = `default-src 'self'; script-src 'self' https://www.ridereadybike.com ; img-src 'self' https://www.ridereadybike.com; connect-src 'self' https://www.ridereadybike.com http://www.strava.com https://vercel.live/; frame-src https://vercel.live/ ; font-src 'self'; style-src 'self';`;
+  cspContent = `default-src 'self'; script-src 'self' https://www.ridereadybike.com ; img-src 'self' https://www.ridereadybike.com; connect-src 'self' https://www.ridereadybike.com http://www.strava.com https://vercel.live/; frame-src https://vercel.live/ ; font-src 'self' https://fonts.gstatic.com/; style-src 'self' https://fonts.gstatic.com/;`;
 }
 
 document.getElementById('csp-meta-tag').setAttribute('content', cspContent);
