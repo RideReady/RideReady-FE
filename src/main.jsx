081c83b0698eb77dd5f9e7ab41485847cea3451a
Rick Vermeil <rickv85@gmail.com>
2023-10-25 08:10:18 -0600

    Add prod and dev .env files, CSP for each

--- src/main.jsx
+++ src/main.jsx
@@ -9,6 +9,20 @@ if (window.location.host === 'www.ridereadybike.com') {
   inject();
 }
 
+let cspContent = "";
+
+const environment = import.meta.env.VITE_ENV;
+
+const fontStylesCSP = "font-src 'self' https://fonts.googleapis.com https://fonts.gstatic.com; style-src 'self' https://fonts.googleapis.com";
+
+if (environment === 'development') {
+  cspContent = `default-src 'self' 'unsafe-inline'; script-src 'self' 'unsafe-inline' http://localhost:5173; img-src 'self' http://localhost:5173; connect-src 'self' http://localhost:5173 http://localhost:5001 http://www.strava.com ; ${fontStylesCSP}`;
+} else {
+  cspContent = `default-src 'self'; script-src 'self' https://www.ridereadybike.com; img-src 'self' https://www.ridereadybike.com; connect-src 'self' https://www.ridereadybike.com http://www.strava.com ; ${fontStylesCSP}`;
+}
+
+document.getElementById('csp-meta-tag').setAttribute('content', cspContent);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
