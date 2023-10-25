ea56fcb82ae90d6e6fea7d5eaafbc057be879181
Rick Vermeil <rickv85@gmail.com>
2023-10-25 08:45:21 -0600

    Remove google fonts

--- src/main.jsx
+++ src/main.jsx
@@ -13,12 +13,10 @@ let cspContent = "";
 
 const environment = import.meta.env.VITE_ENV;
 
-const fontStylesCSP = "font-src 'self' https://fonts.googleapis.com https://fonts.gstatic.com; style-src 'self' https://fonts.googleapis.com";
-
 if (environment === 'development') {
-  cspContent = `default-src 'self' 'unsafe-inline'; script-src 'self' 'unsafe-inline' http://localhost:5173; img-src 'self' http://localhost:5173; connect-src 'self' http://localhost:5173 http://localhost:5001 http://www.strava.com ; ${fontStylesCSP}`;
+  cspContent = `default-src 'self' 'unsafe-inline'; script-src 'self' 'unsafe-inline' http://localhost:5173; img-src 'self' http://localhost:5173; connect-src 'self' http://localhost:5173 http://localhost:5001 http://www.strava.com ; font-src 'self' style-src 'self';`;
 } else {
-  cspContent = `default-src 'self'; script-src 'self' https://www.ridereadybike.com 'sha256-WjvV57CIhmLZyoSLYrfd3e+xHWTmqUKA4z7SJ8eHXIQ='; img-src 'self' https://www.ridereadybike.com; connect-src 'self' https://www.ridereadybike.com http://www.strava.com https://vercel.live/; frame-src https://vercel.live/ ; ${fontStylesCSP}`;
+  cspContent = `default-src 'self'; script-src 'self' https://www.ridereadybike.com ; img-src 'self' https://www.ridereadybike.com; connect-src 'self' https://www.ridereadybike.com http://www.strava.com https://vercel.live/; frame-src https://vercel.live/ ; font-src 'self' style-src 'self';`;
 }
 
 document.getElementById('csp-meta-tag').setAttribute('content', cspContent);
